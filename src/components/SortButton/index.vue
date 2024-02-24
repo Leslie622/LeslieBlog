16ac8dd2cdec214a62a3304edd027846dfc4006f
Leslie <416317444@qq.com>
2024-02-24 19:38:11 +0800

    chore:BlogConfig组件样式调整

--- src/components/SortButton/index.vue
+++ src/components/SortButton/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sort" @click="sortHandler">
-    <div class="sort__content">
+    <div class="content">
       <div class="text">
         <span>{{ text }}</span>
       </div>
@@ -49,60 +49,85 @@ function sortHandler() {
 <style lang="scss" scoped>
 .sort {
   display: inline-block;
-  border-radius: 3px;
   overflow: hidden;
-  cursor: pointer;
+  border-radius: 3px;
   box-shadow: 0 0 0 1px #dcdfe6 inset;
+  cursor: pointer;
+}
 
-  .sort__content {
-    height: 30px;
-    display: flex;
-  }
+.content {
+  height: 1.9rem;
+  display: flex;
+}
 
-  .text,
-  .icon {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.text,
+.icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .text {
-    font-size: 12px;
-    padding: 0.5rem;
-  }
+.text {
+  font-size: 0.75rem;
+  padding: 0.5rem;
+}
 
-  .icon {
-    padding-right: 0.5rem;
-    .icon__content {
-      display: inline-flex;
-      flex-direction: column;
-      position: relative;
-      height: 14px;
-      width: 10px;
+.icon {
+  padding-right: 0.5rem;
+  .icon__content {
+    display: inline-flex;
+    flex-direction: column;
+    position: relative;
+    height: 14px;
+    width: 10px;
 
-      .sort-caret {
-        width: 0;
-        height: 0;
-        border: solid 5px transparent;
-        position: absolute;
-      }
+    .sort-caret {
+      width: 0;
+      height: 0;
+      border: solid 5px transparent;
+      position: absolute;
+    }
 
-      .ascending {
-        top: -4px;
-        border-bottom-color: #dcdfe6;
-        &.active {
-          border-bottom-color: #222222;
-        }
-      }
-
-      .deascending {
-        bottom: -4px;
-        border-top-color: #dcdfe6;
-        &.active {
-          border-top-color: #222222;
-        }
+    .ascending {
+      top: -4px;
+      border-bottom-color: #dcdfe6;
+      &.active {
+        border-bottom-color: #222222;
       }
     }
+
+    .deascending {
+      bottom: -4px;
+      border-top-color: #dcdfe6;
+      &.active {
+        border-top-color: #222222;
+      }
+    }
+  }
+}
+
+/**
+ * ——————————————————响应式——————————————————
+ */
+
+//窗口宽度小于770px
+@media screen and (max-width: 770px) {
+  .icon {
+    transform: scale(0.9);
+  }
+}
+
+//窗口宽度小于500px
+@media screen and (max-width: 500px) {
+  .icon {
+    transform: scale(0.7);
+  }
+}
+
+//窗口宽度小于400px
+@media screen and (max-width: 400px) {
+  .icon {
+    transform: scale(0.65);
   }
 }
 </style>
