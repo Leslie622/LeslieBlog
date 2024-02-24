70ea983b4332e5982b1d928886b5e94bfa73c842
Leslie <416317444@qq.com>
2024-02-24 17:41:48 +0800

    chore:blog页面样式调整

--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="blog">
-    <div class="blog__content">
-      <header class="content__header" :class="{ 'header--active': headerActive }">
+    <div class="content">
+      <header class="header" :class="{ 'header--active': headerActive }">
         <div class="header__inner">
           <div class="inner__block">
-            <div class="switch">
-              <div class="switch__btn" @click="headerActive = !headerActive">
+            <div class="reactive-header">
+              <div class="switch" @click="headerActive = !headerActive">
                 <div>
                   <span class="switch__icon"></span>
                   <span class="switch__icon"></span>
@@ -50,105 +50,106 @@ const headerActive = ref<boolean>(false)
   overflow: hidden;
 }
 
-.blog__content {
+.content {
   margin: 0 auto;
-  width: 1100px; //版心
+  //版心
+  width: 1100px;
 }
 
-.content__header {
+.header {
   position: sticky;
   top: 0;
   height: 0;
 
-  .header__inner {
-    width: 240px;
-    font-size: 14px;
+  /**
+   * switch的动画：
+   * 位移直接过渡，过渡时间为0.2s。
+   * 旋转等待位移过渡完成，等待时间为0.2s 过渡时间为0.1s
+   */
+  &.header--active {
+    // 这里的高度需要大于头部内容总高度，从而通过max-height实现不定高过渡动画
+    max-height: 720px;
 
-    .inner__block {
-      margin-bottom: 10px;
-      padding-bottom: 5px;
-      background-color: var(--blog-block-color);
-      box-shadow: var(--boxShadow);
-      transition:
-        background-color 0.5s,
-        box-shadow 0.5s;
+    .switch__icon:nth-child(1) {
+      transform: translateY(8px);
+      transition: transform 0.2s ease !important;
     }
-
-    .switch {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 130px;
-      color: #fff;
-      background-color: var(--logo-backgorund);
-      transition: background-color 0.5s;
-
-      .switch__btn {
-        position: absolute;
-        display: none;
-
-        left: 20px;
-        width: 40px;
-        height: 40px;
-        cursor: pointer;
-
-        .switch__icon {
-          display: block;
-          padding: 3px 0;
-          transition: transform 0.2s ease 0.1s;
-        }
-
-        .switch__icon:nth-child(2) {
-          opacity: 1;
-          transition: opacity 0.2s ease 0.1s;
-        }
-
-        .switch__icon::before {
-          content: '';
-          display: block;
-          width: 28px;
-          height: 2px;
-          border-radius: 2px;
-          background-color: #fff;
-          transition: transform 0.1s ease;
-        }
-      }
-
-      .logo {
-        font-family: Arial;
-        font-size: 1.3rem;
-      }
+    .switch__icon:nth-child(1)::before {
+      transform: rotate(45deg);
+      transition: transform 0.1s ease 0.2s !important;
+    }
+    .switch__icon:nth-child(2) {
+      opacity: 0 !important;
+    }
+    .switch__icon:nth-child(3) {
+      transform: translateY(-8px);
+      transition: transform 0.2s ease !important;
+    }
+    .switch__icon:nth-child(3)::before {
+      transform: rotate(-45deg);
+      transition: transform 0.1s ease 0.2s !important;
     }
   }
 }
 
-.content__header.header--active {
-  // 这里的高度需要大于头部内容总高度，从而通过max-height实现不定高过渡动画
-  max-height: 720px;
-  /**
-   * switch的动画
-   * 位移直接过渡，过渡时间为0.2s，旋转等待位移过渡完成，等待时间为0.2s 过渡时间为0.1s
-   */
-  .switch__icon:nth-child(1) {
-    transform: translateY(8px);
-    transition: transform 0.2s ease !important;
+.header__inner {
+  width: 240px;
+}
+
+.inner__block {
+  margin-bottom: 10px;
+  padding-bottom: 5px;
+  background-color: var(--blog-block-color);
+  box-shadow: var(--boxShadow);
+  transition:
+    background-color 0.5s,
+    box-shadow 0.5s;
+}
+
+.reactive-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 130px;
+  color: #fff;
+  background-color: var(--logo-backgorund);
+  transition: background-color 0.5s;
+
+  .switch {
+    position: absolute;
+    display: none;
+
+    left: 20px;
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+
+    .switch__icon {
+      display: block;
+      padding: 3px 0;
+      transition: transform 0.2s ease 0.1s;
+    }
+
+    .switch__icon:nth-child(2) {
+      opacity: 1;
+      transition: opacity 0.2s ease 0.1s;
+    }
+
+    .switch__icon::before {
+      content: '';
+      display: block;
+      width: 28px;
+      height: 2px;
+      border-radius: 2px;
+      background-color: #fff;
+      transition: transform 0.1s ease;
+    }
   }
-  .switch__icon:nth-child(1)::before {
-    transform: rotate(45deg);
-    transition: transform 0.1s ease 0.2s !important;
-  }
-  .switch__icon:nth-child(2) {
-    opacity: 0 !important;
-    // transition: opacity 0.2s ease  !important;
-  }
-  .switch__icon:nth-child(3) {
-    transform: translateY(-8px);
-    transition: transform 0.2s ease !important;
-  }
-  .switch__icon:nth-child(3)::before {
-    transform: rotate(-45deg);
-    transition: transform 0.1s ease 0.2s !important;
-  }
+}
+
+.logo {
+  font-family: Arial;
+  font-size: 1.3rem;
 }
 
 .router-view {
@@ -159,10 +160,10 @@ const headerActive = ref<boolean>(false)
  * —————————————— 响应式 ——————————————
  */
 
-// //窗口宽度小于1100px
+//窗口宽度小于1100px
 @media screen and (max-width: 1100px) {
   //版心缩小到1024px
-  .blog__content {
+  .content {
     width: 1024px;
   }
 }
@@ -170,37 +171,33 @@ const headerActive = ref<boolean>(false)
 //窗口宽度小于1024px
 @media screen and (max-width: 1024px) {
   //版心100%
-  .blog__content {
+  .content {
     width: 100%;
   }
 
   //header高度调整
-  .content__header {
+  .header {
     height: auto;
     max-height: 80px;
     overflow: hidden;
     transition: max-height 0.5s;
+  }
 
-    .header__inner {
-      width: 100%;
+  .header__inner {
+    width: 100%;
+  }
 
-      .inner__block {
-        margin-bottom: 0;
-        box-shadow: none;
+  .inner__block {
+    margin-bottom: 0;
+    box-shadow: none;
+  }
 
-        .config__content {
-          padding: 0 2rem;
-        }
-      }
-
-      .switch {
-        height: 80px;
-        .switch__btn {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-      }
+  .reactive-header {
+    height: 80px;
+    .switch {
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
   }
 
